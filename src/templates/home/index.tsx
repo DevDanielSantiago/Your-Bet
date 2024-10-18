9b7ce137da3af930be9abca8d07b399f3e225e7a
Daniel.Souza <daniel.souza@grupoicts.com.br>
2024-10-18 11:35:35 -0400

    feat: adicionar template da pagina inicial

---
+++ src/templates/home/index.tsx
@@ -0,0 +1,13 @@
+import Header from 'shared/components/Header'
+
+import * as S from './styles'
+
+const HomeTemplate = () => {
+  return (
+    <Header>
+      <S.Menu />
+    </Header>
+  )
+}
+
+export default HomeTemplate
---
+++ src/templates/home/styles.ts
@@ -0,0 +1,11 @@
+import MenuIcon from '@mui/icons-material/Menu'
+import styled from 'styled-components'
+
+export const Menu = styled(MenuIcon)`
+  font-size: 50px;
+  color: #676767;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
