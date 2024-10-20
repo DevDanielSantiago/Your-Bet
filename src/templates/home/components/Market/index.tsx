ccaa578cec0ce17c6aff307ab51e7858841f069f
Daniel.Souza <daniel.souza@grupoicts.com.br>
2024-10-20 18:55:13 -0400

    feat: implementar adição de aposta selecionada

--- src/templates/home/components/Market/index.tsx
+++ src/templates/home/components/Market/index.tsx
@@ -1,13 +1,21 @@
-import { MarketType } from 'types/events'
+import { MarketSelectionType, MarketType } from 'types/events'
 
 import * as S from './styles'
+import { useBetStore } from 'stores/bet'
 
 interface MarketProps {
   market: MarketType
   isLast: boolean
+  idEvent: string
 }
 
-const Market = ({ market, isLast }: MarketProps) => {
+const Market = ({ market, isLast, idEvent }: MarketProps) => {
+  const { addSelection, selections } = useBetStore()
+
+  const handleMarket = (market: MarketSelectionType) => () => {
+    addSelection(market)
+  }
+
   return (
     <S.Root key={market.id}>
       <S.Content isLast={isLast}>
@@ -15,16 +23,32 @@ const Market = ({ market, isLast }: MarketProps) => {
           <S.NameMarket>{market.name}</S.NameMarket>
         </S.Header>
         <S.Selections>
-          {market.selections.map((selection) => (
-            <S.Selection key={selection.id} isSelected={false}>
-              <S.SelectionName isSelected={false}>
-                {selection.name}
-              </S.SelectionName>
-              <S.SelectionOdd isSelected={false}>
-                {selection.price}
-              </S.SelectionOdd>
-            </S.Selection>
-          ))}
+          {market.selections.map((selection) => {
+            const idSelected = selections.find(
+              (value) =>
+                value.idEvent === idEvent &&
+                value.idMarket === market.id &&
+                value.idSelection === selection.id
+            )
+            return (
+              <S.Selection
+                key={selection.id}
+                isSelected={!!idSelected}
+                onClick={handleMarket({
+                  idEvent: idEvent,
+                  idMarket: market.id,
+                  idSelection: selection.id
+                })}
+              >
+                <S.SelectionName isSelected={!!idSelected}>
+                  {selection.name}
+                </S.SelectionName>
+                <S.SelectionOdd isSelected={!!idSelected}>
+                  {selection.price}
+                </S.SelectionOdd>
+              </S.Selection>
+            )
+          })}
         </S.Selections>
       </S.Content>
     </S.Root>
