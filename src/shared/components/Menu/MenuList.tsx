6c1d26d7d39e50a64e3f2b9a7022c7278be2ee07
Daniel.Souza <daniel.souza@grupoicts.com.br>
2024-10-20 20:26:34 -0400

    refactor: melhorias no menu de listagem de apostas

--- src/shared/components/Menu/MenuList.tsx
+++ src/shared/components/Menu/MenuList.tsx
@@ -1,3 +1,14 @@
-export function MenuList() {
-  return <div style={{ marginTop: '60px' }}></div>
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+
+interface MenuListProps {
+  children: ReactNode
+}
+
+const List = styled.div`
+  margin: 60px 20px 0px 20px;
+`
+
+export function MenuList({ children }: MenuListProps) {
+  return <List>{children}</List>
 }
