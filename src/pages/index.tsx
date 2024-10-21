2dc82cf1e410cc7ad55cf658fae22019ec2b744f
Daniel.Souza <daniel.souza@grupoicts.com.br>
2024-10-20 20:56:26 -0400

    feat: definir lista de eventos no inicio da página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react'
+
+import { Events } from 'mocks/events'
+import { useBetStore } from 'stores/bet'
+
 import HomeTemplate from 'templates/home'
 
 export default function HomePage() {
+  const { setEvents } = useBetStore()
+
+  useEffect(() => {
+    setEvents(Events)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return <HomeTemplate />
 }
