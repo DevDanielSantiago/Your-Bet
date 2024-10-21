401135ee382993073f50bc8a8f6b219bb32a12ce
Daniel.Souza <daniel.souza@grupoicts.com.br>
2024-10-20 20:53:18 -0400

    feat: implementação dos eventos e ações

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -3,9 +3,14 @@ import { atom, useAtom } from 'jotai'
 
 import Header from 'shared/components/Header'
 import { Menu } from 'shared/components/Menu'
+import Divider from 'shared/components/Divider'
+import Content from './components/Content'
 
-import { IconButton } from '@mui/material'
-import { Close } from '@mui/icons-material'
+import Event from './components/Events'
+import Market from './components/Market'
+import Bets from './components/Bets'
+
+import { useBetStore } from 'stores/bet'
 
 import * as S from './styles'
 
@@ -13,30 +18,57 @@ const statusMenu = atom(false)
 
 const HomeTemplate = () => {
   const [openMenu, setOpenMenu] = useAtom(statusMenu)
+  const { events, selections } = useBetStore()
 
   const toggleMenu = (status: boolean) => () => {
     setOpenMenu(status)
   }
 
   return (
-    <Header>
-      <S.Menu onClick={toggleMenu(true)} />
+    <Fragment>
+      <Header>
+        <S.Menu onClick={toggleMenu(true)} />
 
-      <Menu.Root anchor="right" open={openMenu} onClose={toggleMenu(false)}>
-        <Menu.Content sx={{ width: 315 }}>
-          <Fragment>
-            <IconButton
-              aria-label="close"
-              onClick={toggleMenu(false)}
-              sx={{ position: 'absolute', right: 8, top: 8 }}
-            >
-              <Close sx={{ fontSize: '30px' }} />
-            </IconButton>
-            <Menu.List></Menu.List>
+        <Menu.Root anchor="right" open={openMenu} onClose={toggleMenu(false)}>
+          <Menu.Content sx={{ maxWidth: '315px', width: 315 }}>
+            <Fragment>
+              <S.Icon aria-label="close" onClick={toggleMenu(false)}>
+                <S.Close />
+              </S.Icon>
+              <Menu.List>
+                {selections.map((bet, index) => (
+                  <Fragment key={index}>
+                    <Bets selection={bet} />
+                    {selections.length > 1 &&
+                      index >= 0 &&
+                      index <= selections.length - 2 && <Divider />}
+                  </Fragment>
+                ))}
+              </Menu.List>
+            </Fragment>
+          </Menu.Content>
+        </Menu.Root>
+      </Header>
+
+      <Content>
+        {events.map((event) => (
+          <Fragment key={event.id}>
+            {event.markets.length > 0 && (
+              <Event name={event.name}>
+                {event.markets.map((market, index) => (
+                  <Market
+                    key={market.id}
+                    idEvent={event.id}
+                    market={market}
+                    isLast={event.markets.length - 1 === index}
+                  />
+                ))}
+              </Event>
+            )}
           </Fragment>
-        </Menu.Content>
-      </Menu.Root>
-    </Header>
+        ))}
+      </Content>
+    </Fragment>
   )
 }
 
--- src/templates/home/styles.ts
+++ src/templates/home/styles.ts
@@ -1,4 +1,7 @@
 import MenuIcon from '@mui/icons-material/Menu'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
+
 import styled from 'styled-components'
 
 export const Menu = styled(MenuIcon)`
@@ -8,4 +11,14 @@ export const Menu = styled(MenuIcon)`
   &:hover {
     cursor: pointer;
   }
+`
+
+export const Icon = styled(IconButton)`
+  position: absolute;
+  right: 8px;
+  top: 8px;
+`
+
+export const Close = styled(CloseIcon)`
+  font-size: 30px;
 `
